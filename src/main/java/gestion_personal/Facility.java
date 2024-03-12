2f8fd68c6cc49bf21cf7ead23bfca59e056483df
aalvaroo7 <almarrom@myuax.com>
2024-03-12 12:00:57 +0100

    yiiipie

--- src/main/java/gestion_personal/Facility.java
+++ src/main/java/gestion_personal/Facility.java
@@ -1,4 +1,51 @@
 package gestion_personal;
-
+import java.util.Date;
 public class Facility {
+    private int id;
+    private String name;
+    private String location;
+    private Date lastMaintenanceDate;
+    private boolean urgentRepairNeeded;
+
+    // getters
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public Date getLastMaintenanceDate() {
+        return this.lastMaintenanceDate;
+    }
+
+    public boolean isUrgentRepairNeeded() {
+        return this.urgentRepairNeeded;
+    }
+
+    // setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setLastMaintenanceDate(Date lastMaintenanceDate) {
+        this.lastMaintenanceDate = lastMaintenanceDate;
+    }
+
+    public void setUrgentRepairNeeded(boolean urgentRepairNeeded) {
+        this.urgentRepairNeeded = urgentRepairNeeded;
+    }
 }
