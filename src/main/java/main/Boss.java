fa29576810219a87d3d9828e639963b852c99b90
aalvaroo7 <almarrom@myuax.com>
2024-03-12 16:56:01 +0100

    parte identificacion

--- src/main/java/main/Boss.java
+++ src/main/java/main/Boss.java
@@ -1,30 +1,33 @@
+package main;
+
 import gestion_habitats.habitats;
 import gestion_personal.Camera;
-import gestion_personal.Resource;
+import gestion_recursos.Resource;
 import java.util.List;
+public class Boss {
+    private List<gestion_habitats.habitats> habitats;
+    private List<Camera> cameras;
+    private List<Resource> resources;
 
-private List<gestion_habitats.habitats> habitats;
-private List<Camera> cameras;
-private List<Resource> resources;
+    public Boss(List<habitats> habitats, List<Camera> cameras, List<Resource> resources) {
+        this.habitats = habitats;
+        this.cameras = cameras;
+        this.resources = resources;
+    }
 
-public Boss(List<habitats> habitats, List<Camera> cameras, List<Resource> resources) {
-    this.habitats = habitats;
-    this.cameras = cameras;
-    this.resources = resources;
-}
+    public String identify() {
+        return "Boss";
+    }
 
-public String identify() {
-    return "Boss";
-}
+    public List<habitats> getHabitats() {
+        return habitats;
+    }
 
-public List<habitats> getHabitats() {
-    return habitats;
-}
+    public List<Camera> getCameras() {
+        return cameras;
+    }
 
-public List<Camera> getCameras() {
-    return cameras;
-}
-
-public List<Resource> getResources() {
-    return resources;
+    public List<Resource> getResources() {
+        return resources;
+    }
 }
