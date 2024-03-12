c8db40d9d0d334e84bf32d7dffc252f6bc88f868
aalvaroo7 <almarrom@myuax.com>
2024-03-12 11:10:45 +0100

    yiiipie

---
+++ src/main/java/gestion_recursos/Order.java
@@ -0,0 +1,10 @@
+package gestion_recursos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private List<Resource> resources = new ArrayList<>();
+
+    // methods to add resources to the order
+}
