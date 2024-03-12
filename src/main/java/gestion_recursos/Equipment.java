477e71185be55b693ebf0a4e5f543598066feb15
aalvaroo7 <almarrom@myuax.com>
2024-03-12 11:05:41 +0100

    yiiipie

---
+++ src/main/java/gestion_recursos/Equipment.java
@@ -0,0 +1,7 @@
+package gestion_recursos;
+
+public class Equipment extends Resource {
+    public Equipment(String name, int quantity, Supplier supplier) {
+        super(name, quantity, supplier);
+    }
+}
