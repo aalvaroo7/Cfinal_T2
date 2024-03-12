08c855d527d1b38b133f7d81450eb30fa8d03282
aalvaroo7 <almarrom@myuax.com>
2024-03-12 12:03:17 +0100

    yiiipie

--- src/main/java/gestion_personal/MaintenanceTracker.java
+++ src/main/java/gestion_personal/MaintenanceTracker.java
@@ -1,4 +1,22 @@
 package gestion_personal;
 
+import java.util.List;
 public class MaintenanceTracker {
+    private List<Facility> facilities;
+    private List<Maintenance> maintenances;
+    private List<Repair> repairs;
+
+    public void addFacility(Facility facility) {
+        // add facility to facilities
+    }
+
+    public void scheduleMaintenance(Maintenance maintenance) {
+        // add maintenance to maintenances
+    }
+
+    public void registerRepair(Repair repair) {
+        // add repair to repairs
+    }
+
+    // other methods to track maintenance and repairs
 }
