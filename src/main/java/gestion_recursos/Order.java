971b75b3402a623c86846b4d11312460fdc91b7e
aalvaroo7 <almarrom@myuax.com>
2024-03-12 11:14:51 +0100

    yiiipie

--- src/main/java/gestion_recursos/Order.java
+++ src/main/java/gestion_recursos/Order.java
@@ -1,10 +1,21 @@
 package gestion_recursos;
-
 import java.util.ArrayList;
 import java.util.List;
-
 public class Order {
     private List<Resource> resources = new ArrayList<>();
 
-    // methods to add resources to the order
+    // Method to add a resource
+    public void addResource(Resource resource) {
+        resources.add(resource);
+    }
+
+    // Method to remove a resource
+    public void removeResource(Resource resource) {
+        resources.remove(resource);
+    }
+
+    // Method to check if a resource is in the order
+    public boolean checkResource(Resource resource) {
+        return resources.contains(resource);
+    }
 }
