ad5d546ebc319284e9db264715ba2e5fa86f01e1
aalvaroo7 <almarrom@myuax.com>
2024-03-12 17:18:44 +0100

    parte identificacion

--- src/main/java/main/Staff.java
+++ src/main/java/main/Staff.java
@@ -1,7 +1,27 @@
 package main;
 
+import gestion_recursos.Resource;
+import animales.animal;
+import java.util.List;
+
 public class Staff extends Person {
-      public String identify() {
+    private List<Resource> resources;
+    private List<animal> animals;
+
+    public Staff(List<Resource> resources, List<animal> animals) {
+        this.resources = resources;
+        this.animals = animals;
+    }
+
+    public String identify() {
         return "Staff";
+    }
+
+    public List<Resource> getResources() {
+        return resources;
+    }
+
+    public List<animal> getAnimals() {
+        return animals;
     }
 }
