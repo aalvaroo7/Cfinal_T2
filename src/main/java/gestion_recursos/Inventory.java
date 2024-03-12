f7a2d8f634eba656ca88845f64926c3bcc432eed
aalvaroo7 <almarrom@myuax.com>
2024-03-12 11:09:38 +0100

    yiiipie

--- src/main/java/gestion_recursos/Inventory.java
+++ src/main/java/gestion_recursos/Inventory.java
@@ -1,3 +1,6 @@
+package gestion_recursos;
+import java.util.ArrayList;
+import java.util.List;
 public class Inventory {
     private List<Food> foodList = new ArrayList<>();
     private List<Medicine> medicineList = new ArrayList<>();
