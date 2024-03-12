6cf9f3bd5193e2517c880b4e68a2f5f10f39473a
aalvaroo7 <almarrom@myuax.com>
2024-03-12 12:02:32 +0100

    yiiipie

--- src/main/java/gestion_personal/Repair.java
+++ src/main/java/gestion_personal/Repair.java
@@ -1,4 +1,50 @@
 package gestion_personal;
+import java.util.Date;
 
 public class Repair {
+    private int id;
+    private int facilityId;
+    private Date repairDate;
+    private String repairType;
+    private boolean isUrgent;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFacilityId() {
+        return this.facilityId;
+    }
+
+    public void setFacilityId(int facilityId) {
+        this.facilityId = facilityId;
+    }
+
+    public Date getRepairDate() {
+        return this.repairDate;
+    }
+
+    public void setRepairDate(Date repairDate) {
+        this.repairDate = repairDate;
+    }
+
+    public String getRepairType() {
+        return this.repairType;
+    }
+
+    public void setRepairType(String repairType) {
+        this.repairType = repairType;
+    }
+
+    public boolean isUrgent() {
+        return this.isUrgent;
+    }
+
+    public void setUrgent(boolean isUrgent) {
+        this.isUrgent = isUrgent;
+    }
 }
