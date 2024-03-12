131c21724a83c2f046d6e9dcca34a7939f2b45a7
aalvaroo7 <almarrom@myuax.com>
2024-03-12 21:18:29 +0100

    me muero

--- src/main/java/gestion_recursos/Medicine.java
+++ src/main/java/gestion_recursos/Medicine.java
@@ -2,6 +2,6 @@ package gestion_recursos;
 
 public class Medicine extends Resource {
     public Medicine(String name, int quantity, Supplier supplier) {
-        super(name, quantity, supplier);
+        super(name, quantity, supplier, null, null, null);
     }
 }
