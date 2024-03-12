76ec9e6bcc0802ad3cc57fa0833010ad12a7e82d
aalvaroo7 <almarrom@myuax.com>
2024-03-12 10:59:09 +0100

    yiiipie

--- src/main/java/animales/Maintenance.java
+++ src/main/java/animales/Maintenance.java
@@ -7,27 +7,35 @@ public class Maintenance {
         this.name = name;
     }
 
-    public void manageAnimal(animales animal) {
+    public void manageAnimal(animal animal) {
         // logic to manage an animal
-        if (animal instanceof animal_acuaticos) {
+        if (animal instanceof animal_acuatico) {
             // logic to manage aquatic animals
-        } else if (animal instanceof animal_terrestres) {
+        } else if (animal instanceof animal_terrestre) {
             // logic to manage terrestrial animals
-        } else if (animal instanceof animal_aviarios) {
+        } else if (animal instanceof animal_aviario) {
             // logic to manage aviary animals
         }
     }
 
-    public void careForAnimal(animales animal) {
+    public void careForAnimal(animal animal) {
         // logic to care for an animal
-        if (animal instanceof animal_acuaticos) {
+        if (animal instanceof animal_acuatico) {
             // logic to care for aquatic animals
-        } else if (animal instanceof animal_terrestres) {
+        } else if (animal instanceof animal_terrestre) {
             // logic to care for terrestrial animals
-        } else if (animal instanceof animal_aviarios) {
+        } else if (animal instanceof animal_aviario) {
             // logic to care for aviary animals
         }
     }
 
-    // getters and setters
+    // Getter for name
+    public String getName() {
+        return this.name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
 }
